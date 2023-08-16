361ef5f7a66a5e09b029d83596747ab86547545b
Adexlexy <oshewolo_martins@yahoo.com>
2023-08-16 15:54:49 +0000

    Redis

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+"""Contains a cache class"""
+
+from typing import Union, Callable
+from uuid import uuid4
+
+import redis
+
+
+class Cache:
+    """
+    A cache class
+    """
+    def __init__(self):
+        """
+        Initializes the cache class
+        Args:
+            redis: Instance of the redis class
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        Generate random id, stes it as the key and use the data
+        passed to the function as the value
+        """
+        key: str = str(uuid4())
+        self._redis.set(key, data)
+        return key
+
+    def get(self, key: str, fn: Callable = None) -> [int, bytes, float, str]:
+        """
+        convert to any desired format
+        """
+
+        data = self._redis.get(key)
+        if fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """
+        REtrieves a string from redis using the provided key and
+        returns it as a sring
+        Args:
+            key: str retrieved from redis
+        Return:
+                str
+        """
+        return self.get(key, fn=lambda x: x.decode("utf-8"))
+
+    def get_int(self, key: int) -> int:
+        """
+           REtrieves a int from redis using the provided key and
+        returns it as an integer
+        Args:
+            key: int retrieved from redis
+        Return:
+                int
+        """
+
+        return self.get(key, fn=lambda x: int(x))
+
